87e05c98fdab31d6d7fd8e824fa263fccf04091c
Evangelos Paterakis <evan@geopjr.dev>
2021-07-02 02:10:48 +0300

    fix(translation): format EL

---
+++ translations/el.js
@@ -0,0 +1,75 @@
+// Greek translation for dev.geopjr.Hashbrown.
+// Copyright (C) 2021 THE dev.geopjr.Hashbrown'S COPYRIGHT HOLDER
+// This file is distributed under the same license as the dev.geopjr.Hashbrown package.
+// Evangelos Paterakis <evan@geopjr.dev>, 2021.
+
+// Last-Translator: Evangelos Paterakis <evan@geopjr.dev>
+// Language: el
+
+module.exports = {
+  "About Hashbrown": {
+    comment: "This is a button in the menu dropdown.",
+    translation: "Σχετικά με το Hashbrown",
+  },
+  "Generate, compare and verify MD5, SHA256 & SHA1 hashes.": {
+    comment: "Subtext in welcomer",
+    translation:
+      "Δημιούργησε, σύγκρινε και Επαλήθευσε MD5, SHA256 & SHA1 hashes.",
+  },
+  "Select a File": {
+    comment: "Tooltip for the main file chooser.",
+    translation: "Επίλεξε ένα Αρχείο",
+  },
+  "Choose a File": {
+    comment: "Title for the main file chooser dialog.",
+    translation: "Επίλεξε ένα Αρχείο",
+  },
+  Copy: {
+    comment: "A copy text button.",
+    translation: "Αντιγραφή",
+  },
+  Generate: {
+    comment: "Header tab about generating hashes.",
+    translation: "Δημιούργησε",
+  },
+  "Select Another File": {
+    comment: "Tooltip for the second file chooser under the compare tab.",
+    translation: "Επίλεξε ένα άλλο Αρχείο",
+  },
+  "Choose Another File": {
+    comment: "Title for the 'compare' file chooser dialog.",
+    translation: "Επίλεξε ένα άλλο Αρχείο",
+  },
+  Compare: {
+    comment:
+      "Label of a button that compares two files. Also header tab about comparing files (their hashes).",
+    translation: "Σύγκρινε",
+  },
+  "Insert a MD5/SHA1/SHA256 Hash": {
+    comment: "Tooltip for the hash text input under the verify tab.",
+    translation: "Είσαγε ένα MD5/SHA1/SHA256 Hash",
+  },
+  "MD5/SHA1/SHA256 Hash": {
+    comment:
+      "Placeholder text for an input box. Only translate 'hash' if possible.",
+    translation: "MD5/SHA1/SHA256 Hash",
+  },
+  Verify: {
+    comment:
+      "Label of a button that  verifies that a hash belongs to a selected file. Also header tab about verifing that a hash belongs to a selected file.",
+    translation: "Επαλήθευσε",
+  },
+  Menu: {
+    comment: "The headbar menu icon-button tooltip.",
+    translation: "Μενού",
+  },
+  "They match!": {
+    comment: "Message displayed when comparison or verification is successful.",
+    translation: "Ταιριάζουν!",
+  },
+  "They don't match!": {
+    comment:
+      "Message displayed when comparison or verification is not successful.",
+    translation: "Δεν ταιριάζουν!",
+  },
+};
---
+++ translations/hashbrown.js
@@ -0,0 +1,74 @@
+// SOME DESCRIPTIVE TITLE.
+// Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER
+// This file is distributed under the same license as the PACKAGE package.
+// FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
+
+// Last-Translator: FULL NAME <EMAIL@ADDRESS>
+// Language:
+
+module.exports = {
+  "About Hashbrown": {
+    comment: "This is a button in the menu dropdown.",
+    translation: "",
+  },
+  "Generate, compare and verify MD5, SHA256 & SHA1 hashes.": {
+    comment: "Subtext in welcomer",
+    translation: "",
+  },
+  "Select a File": {
+    comment: "Tooltip for the main file chooser.",
+    translation: "",
+  },
+  "Choose a File": {
+    comment: "Title for the main file chooser dialog.",
+    translation: "",
+  },
+  Copy: {
+    comment: "A copy text button.",
+    translation: "",
+  },
+  Generate: {
+    comment: "Header tab about generating hashes.",
+    translation: "",
+  },
+  "Select Another File": {
+    comment: "Tooltip for the second file chooser under the compare tab.",
+    translation: "",
+  },
+  "Choose Another File": {
+    comment: "Title for the 'compare' file chooser dialog.",
+    translation: "",
+  },
+  Compare: {
+    comment:
+      "Label of a button that compares two files. Also header tab about comparing files (their hashes).",
+    translation: "",
+  },
+  "Insert a MD5/SHA1/SHA256 Hash": {
+    comment: "Tooltip for the hash text input under the verify tab.",
+    translation: "",
+  },
+  "MD5/SHA1/SHA256 Hash": {
+    comment:
+      "Placeholder text for an input box. Only translate 'hash' if possible.",
+    translation: "",
+  },
+  Verify: {
+    comment:
+      "Label of a button that  verifies that a hash belongs to a selected file. Also header tab about verifing that a hash belongs to a selected file.",
+    translation: "",
+  },
+  Menu: {
+    comment: "The headbar menu icon-button tooltip.",
+    translation: "",
+  },
+  "They match!": {
+    comment: "Message displayed when comparison or verification is successful.",
+    translation: "",
+  },
+  "They don't match!": {
+    comment:
+      "Message displayed when comparison or verification is not successful.",
+    translation: "",
+  },
+};
