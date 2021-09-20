df1963bc6534d4b9f51f59bd097413fea8f08d33
milotype <43657314+milotype@users.noreply.github.com>
2021-09-20 09:54:08 +0200

    Create hr.js
    Add Croatian translation

---
+++ translations/hr.js
@@ -0,0 +1,74 @@
+// Croatian translation for dev.geopjr.Hashbrown.
+// Copyright (C) 2021 THE dev.geopjjr.Hashbrown'S COPYRIGHT HOLDER
+// This file is distributed under the same license as the dev.geopjr.Hashbrown package.
+// Milo Ivir <mail@milotype.de>, 2021.
+
+// Last-Translator: Milo Ivir <mail@milotype.de>
+// Language: hr
+
+module.exports = {
+  "About Hashbrown": {
+    comment: "This is a button in the menu dropdown.",
+    translation: "Hashbrown informacije",
+  },
+  "Check hashes for your files": {
+    comment: "Subtext in welcomer",
+    translation: "Provjeri hasheve tvojih datoteka",
+  },
+  "Select a File": {
+    comment: "Tooltip for the main file chooser.",
+    translation: "Odaberi datoteku",
+  },
+  "Choose a File": {
+    comment: "Title for the main file chooser dialog.",
+    translation: "Odaberi datoteku",
+  },
+  Copy: {
+    comment: "A copy text button.",
+    translation: "Kopiraj",
+  },
+  Generate: {
+    comment: "Header tab about generating hashes.",
+    translation: "Generiraj",
+  },
+  "Select Another File": {
+    comment: "Tooltip for the second file chooser under the compare tab.",
+    translation: "Odaberi drugu datoteku",
+  },
+  "Choose Another File": {
+    comment: "Title for the 'compare' file chooser dialog.",
+    translation: "Odaberi drugu datoteku",
+  },
+  Compare: {
+    comment:
+      "Label of a button that compares two files. Also header tab about comparing files (their hashes).",
+    translation: "Usporedi",
+  },
+  "Insert a MD5/SHA-1/SHA-256 Hash": {
+    comment: "Tooltip for the hash text input under the verify tab.",
+    translation: "Umetni MD5/SHA-1/SHA-256 hash",
+  },
+  "MD5/SHA-1/SHA-256 Hash": {
+    comment:
+      "Placeholder text for an input box. Only translate 'hash' if possible.",
+    translation: "MD5/SHA-1/SHA-256 hash",
+  },
+  Verify: {
+    comment:
+      "Label of a button that  verifies that a hash belongs to a selected file. Also header tab about verifing that a hash belongs to a selected file.",
+    translation: "Provjeri",
+  },
+  Menu: {
+    comment: "The headbar menu icon-button tooltip.",
+    translation: "Izbornik",
+  },
+  "They match!": {
+    comment: "Message displayed when comparison or verification is successful.",
+    translation: "Poklapaju se!",
+  },
+  "They don't match!": {
+    comment:
+      "Message displayed when comparison or verification is not successful.",
+    translation: "Ne poklapaju se!",
+  },
+};
