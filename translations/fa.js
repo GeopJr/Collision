b2407e7040a379d9106284cebe10a128e6c96b71
MSKF <mskf1383@protonmail.com>
2021-10-17 08:45:47 +0330

    Create fa.js

---
+++ translations/fa.js
@@ -0,0 +1,74 @@
+// SOME DESCRIPTIVE TITLE.
+// Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER
+// This file is distributed under the same license as the PACKAGE package.
+// MohammadSaleh Kamyab <mskf1383@protonmail.com>, 2021.
+
+// Last-Translator: MohammadSaleh Kamyab <mskf1383@protonmail.com>
+// Language: Persian
+
+module.exports = {
+  "About Hashbrown": {
+    comment: "This is a button in the menu dropdown.",
+    translation: "درباره‌ی هش‌براون",
+  },
+  "Check hashes for your files": {
+    comment: "Subtext in welcomer",
+    translation: "هش پرونده‌هایتان را بررسی کنید",
+  },
+  "Select a File": {
+    comment: "Tooltip for the main file chooser.",
+    translation: "انتخاب یک پرونده",
+  },
+  "Choose a File": {
+    comment: "Title for the main file chooser dialog.",
+    translation: "انتخاب یک پرونده",
+  },
+  Copy: {
+    comment: "A copy text button.",
+    translation: "رونوشت",
+  },
+  Generate: {
+    comment: "Header tab about generating hashes.",
+    translation: "تولید",
+  },
+  "Select Another File": {
+    comment: "Tooltip for the second file chooser under the compare tab.",
+    translation: "انتخاب پرونده‌ای دیگر",
+  },
+  "Choose Another File": {
+    comment: "Title for the 'compare' file chooser dialog.",
+    translation: "انتخاب پرونده‌ای دیگر",
+  },
+  Compare: {
+    comment:
+      "Label of a button that compares two files. Also header tab about comparing files (their hashes).",
+    translation: "مقایسه",
+  },
+  "Insert a MD5/SHA-1/SHA-256 Hash": {
+    comment: "Tooltip for the hash text input under the verify tab.",
+    translation: "افزودن یک هش MD5/SHA-1/SHA-256",
+  },
+  "MD5/SHA-1/SHA-256 Hash": {
+    comment:
+      "Placeholder text for an input box. Only translate 'hash' if possible.",
+    translation: "هش MD5/SHA-1/SHA-256",
+  },
+  Verify: {
+    comment:
+      "Label of a button that  verifies that a hash belongs to a selected file. Also header tab about verifing that a hash belongs to a selected file.",
+    translation: "اعتبارسنجی",
+  },
+  Menu: {
+    comment: "The headbar menu icon-button tooltip.",
+    translation: "گزینگان",
+  },
+  "They match!": {
+    comment: "Message displayed when comparison or verification is successful.",
+    translation: "منطبق‌اند!",
+  },
+  "They don't match!": {
+    comment:
+      "Message displayed when comparison or verification is not successful.",
+    translation: "منطبق نیستند!",
+  },
+};
