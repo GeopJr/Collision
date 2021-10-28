0c2906fcc5234ded8455845891a1a07c50b959c4
Evangelos Paterakis <evan@geopjr.dev>
2021-10-28 19:46:37 +0300

    fix(actions)[hashinfo]: run xdg-open directly
    instead of using Gio

--- src/modules/functions/hashinfo_action.cr
+++ src/modules/functions/hashinfo_action.cr
@@ -8,7 +8,10 @@ module Hashbrown
     app.add_action(action)
 
     action.activate_signal.connect do
-      LibGio.g_app_info_launch_default_for_uri(ARTICLE, Pointer(Void).null)
+      # Messes up memory, use xdg-open directly for now
+      # LibGio.g_app_info_launch_default_for_uri(ARTICLE, Pointer(Void).null)
+
+      Hashbrown.run_cmd("xdg-open", [ARTICLE])
     end
   end
 end
