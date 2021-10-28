d381e8bfc44478c2e1e497dc8ea45deb69c17d96
Evangelos Paterakis <evan@geopjr.dev>
2021-10-28 19:45:42 +0300

    fix(switcher): don't rebuild the UI on main file change
    just reset the feedback icons

--- src/modules/functions/icon_name.cr
+++ src/modules/functions/icon_name.cr
@@ -3,6 +3,13 @@
 module Hashbrown
   extend self
 
+  DEFAULT_FEEDBACK_ICON = "dialog-password-symbolic"
+
+  def reset_feedback
+    TOOL_COMPARE_ROW.icon_name = DEFAULT_FEEDBACK_ICON
+    TOOL_VALIDATE_ROW.icon_name = DEFAULT_FEEDBACK_ICON
+  end
+
   def icon(success : Bool? = false)
     success ? "emblem-ok-symbolic" : "window-close-symbolic"
   end
