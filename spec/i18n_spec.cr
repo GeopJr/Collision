c64a9937cce04746138ee33a5f1b8a2e6378cae4
Evangelos Paterakis <evan@geopjr.dev>
2021-10-28 06:38:26 +0300

    chore: specs

--- spec/i18n_spec.cr
+++ spec/i18n_spec.cr
@@ -1,11 +1,26 @@
 require "./spec_helper"
 require "../src/modules/functions/current_lang.cr"
+require "../src/modules/functions/translate_ui.cr"
+
+Hashbrown::TRANSLATIONS = {
+  "el" => {
+    "Hello World" => "Γεία σου Κόσμε",
+  },
+  "en" => Hash(String, String).new,
+  "ru" => Hash(String, String).new,
+}
 
 describe "i18n" do
-  ENV["LANGUAGE"] = "en_US.UTF-8"
+  ENV["LANGUAGE"] = "el:en_US.UTF-8"
   it "gets current language" do
     lang = Hashbrown.current_language
 
-    lang.should eq("en")
+    lang.should eq("el")
+  end
+  it "translates strings" do
+    original = "HASHBROWN_(Hello World)"
+    translated = Hashbrown.translate(original, Hashbrown.current_language)
+
+    translated.should eq(Hashbrown::TRANSLATIONS["el"]["Hello World"])
   end
 end
