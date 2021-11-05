0492ce7016d5f20e2497c6c110cc436c41a6cd47
Evangelos Paterakis <evan@geopjr.dev>
2021-11-05 17:44:52 +0200

    feat(scripts)[thank_translators]: sort by people
    since one translator can contribute in more than once language, it's better to avoid showing them multiple times, but rather list all the languages they contributed to next to them

--- extra/scripts/thank_translators.cr
+++ extra/scripts/thank_translators.cr
@@ -5,7 +5,7 @@ TRANSLATION_DIR_NAME = "translations"
 translations_dir = Dir.open(TRANSLATION_DIR_NAME)
 translations = translations_dir.children.reject! { |x| x == "hashbrown.yaml" || !x.ends_with?(".yaml") }
 
-thanks = [] of String
+thanks = Hash(String, Array(String)).new
 
 regex = {
   "lang"        => /## ?Language\: ?(.+)\n/,
@@ -15,16 +15,25 @@ regex = {
 translations.each do |translation|
   translation_path = Path[TRANSLATION_DIR_NAME].join(translation)
   translation_content = File.read(translation_path)
+
   lang = translation_content.scan(regex["lang"])[0]?.try &.[1]?
   translators = translation_content.scan(regex["translators"])
+
   translators.each do |t_md|
     t_name = t_md[1]?
     next if t_name.nil?
-    thanks_string = "#{t_name}#{lang.nil? ? "" : " (" + lang.upcase + ")"}"
-    thanks << thanks_string
+    thanks[t_name] = [] of String unless thanks.has_key?(t_name)
+    thanks[t_name] << lang.upcase unless lang.nil?
   end
 end
 
-thanks.sort! { |a, b| a.split(" ")[-1] <=> b.split(" ")[-1] }
+thanks = thanks.to_a.sort! { |a, b| a.first <=> b.first }.to_h
+thanks.map { |k, v| v.sort! }
 
-puts thanks.join("\n")
+thanks_block = [] of String
+
+thanks.each do |k, v|
+  thanks_block << "#{k} (#{v.join(", ")})"
+end
+
+puts thanks_block.join("\n")
