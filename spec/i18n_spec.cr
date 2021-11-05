a9cec31043418dd07cc9f212fa5137852a040a52
Evangelos Paterakis <evan@geopjr.dev>
2021-11-05 16:59:04 +0200

    feat: generate some hashes during compile-time

--- spec/i18n_spec.cr
+++ spec/i18n_spec.cr
@@ -2,13 +2,13 @@ require "./spec_helper"
 require "../src/modules/functions/current_lang.cr"
 require "../src/modules/functions/translate_ui.cr"
 
-Hashbrown::TRANSLATIONS = {
+Hashbrown::TRANSLATIONS.merge!({
   "el" => {
     "Hello World" => "Γεία σου Κόσμε",
   },
   "en" => Hash(String, String).new,
   "ru" => Hash(String, String).new,
-}
+})
 
 describe "i18n" do
   ENV["LANGUAGE"] = "el:en_US.UTF-8"
