4a3b64aa34fa4bc098508c28afb5ec88f82281e5
Evangelos Paterakis <evan@geopjr.dev>
2021-11-05 17:13:38 +0200

    feat: fibers
    feat(compare): md5 -> sha256
    
    So far, Hashbrown used the md5sum, sha1sum, sha256sum & sha512sum, however it started causing some memory issues (#20). Hashbrown now uses Crystal's digest functions to calculate the hashes. At the same time it uses Fibers to achieve concurrency, making calculating big files *much* faster!
    
    Compare now uses the SHA256 hashes instead of the MD5 ones as they are more secure.

--- spec/copy_spec.cr
+++ spec/copy_spec.cr
@@ -11,9 +11,11 @@ describe "run_cmd" do
   it "gets hashes from file" do
     path = Path["./spec/test.txt"].expand(home: true)
     hashes = [] of String
-    ["sha512sum", "sha256sum", "md5sum", "sha1sum"].each do |x|
-      output = Hashbrown.run_cmd(x, [path.to_s]).split(" ")[0]
-      hashes << output
+    channel = Channel(Hash(String, String)).new
+
+    ["sha512", "sha256", "md5", "sha1"].each do |x|
+      spawn(Hashbrown.calculate_hash(x, path.to_s, channel))
+      hashes << channel.receive[x.upcase]
     end
 
     hashes.should eq(["2ddb9cca7753a01fcbcdbacc228ae8c314ff4e735c6fffb42286272fb460930dc74f122a9f365053a13016b22403ad4e192142e8668bc5f7d6fce865eb38ec2c", "08e3dfc089e66c44ae8abd3476b0f810f11f4cfc2ab925f1607c7df21345d639", "f7e3f382f0382147661c82af20e274e8", "c8f0b71214e8164aa69419b7ac0bcd8a74f529a6"])
--- src/modules/functions/generate_hash.cr
+++ src/modules/functions/generate_hash.cr
@@ -1,7 +1,19 @@
+require "openssl"
+
+macro gen_digest
+  {
+    {% for hash in Hashbrown::HASH_FUNCTIONS %}
+      {{hash.downcase}} => OpenSSL::Digest.new({{hash.upcase}}),
+    {% end %}
+  }
+end
+
 module Hashbrown
   extend self
 
-  def run_cmd(cmd, args)
+  @@digest = gen_digest
+
+  def run_cmd(cmd, args) : String
     stdout = IO::Memory.new
     stderr = IO::Memory.new
     status = Process.run(cmd, args: args, output: stdout, error: stderr)
@@ -10,15 +22,27 @@ module Hashbrown
     output
   end
 
-  def generate_hashes(filename : String, textFields : Hash(String, Gtk::Entry), copyBtns : Array(Gtk::Button)) : Hash(Gtk::Button, String)
+  def calculate_hash(type : String, filename : String, channel : Channel(Hash(String, String))) : Channel(Hash(String, String))
+    hash = @@digest[type.downcase]
+    hash.reset
+    channel.send({type.upcase => hash.file(filename).final.hexstring.downcase})
+  end
+
+  def generate_hashes(filename : String)
     tempBtns = Hash(Gtk::Button, String).new
-    i = 0
-    textFields.each do |k, v|
-      output = Hashbrown.run_cmd("#{k}", [filename]).split(" ")[0].downcase
-      v.buffer.set_text(output, output.size)
-      tempBtns[copyBtns[i]] = output
-      i = i.succ
+    channel = Channel(Hash(String, String)).new
+
+    TEXT_FIELDS.keys.each do |k|
+      spawn(calculate_hash("#{k}", filename, channel))
     end
-    tempBtns
+
+    (0..TEXT_FIELDS.size - 1).each do |x|
+      output = channel.receive
+      hash_type = output.keys.first
+      hash_value = output[hash_type]
+
+      TEXT_FIELDS[hash_type].buffer.set_text(hash_value, hash_value.size)
+      CLIPBOARD_HASH[hash_type] = hash_value
+    end
   end
 end
