bafc3ef2629abebae02502fddd4f8a48db701ad0
Evangelos Paterakis <evan@geopjr.dev>
2022-03-19 00:07:47 +0200

    feat: handle Gettext#setlocale error
    happens on unknown language, especially on flatpak
    this lets the app run instead of crashing

--- src/modules/prerequisites.cr
+++ src/modules/prerequisites.cr
@@ -3,9 +3,13 @@ module Collision
 
   LOGGER = Log.for("Collision", ARGV[0]? == "--debug" ? Log::Severity::Debug : Log::Severity::Warn)
 
-  Gettext.setlocale(Gettext::LC::ALL, "")
-  Gettext.bindtextdomain("dev.geopjr.Collision", {{env("COLLISION_LOCALE_LOCATION").nil? ? "/usr/share/locale" : env("COLLISION_LOCALE_LOCATION")}})
-  Gettext.textdomain("dev.geopjr.Collision")
+  begin
+    Gettext.setlocale(Gettext::LC::ALL, "")
+    Gettext.bindtextdomain("dev.geopjr.Collision", {{env("COLLISION_LOCALE_LOCATION").nil? ? "/usr/share/locale" : env("COLLISION_LOCALE_LOCATION")}})
+    Gettext.textdomain("dev.geopjr.Collision")
+  rescue ex
+    LOGGER.debug { ex }
+  end
 
   HASH_FUNCTIONS = ["MD5", "SHA1", "SHA256", "SHA512"]
   VERSION        = {{read_file("./shard.yml").split("version: ")[1].split("\n")[0]}} # Shards binary might not be in PATH, reading yml is safer
