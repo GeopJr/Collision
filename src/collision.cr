870798b83195ccd74319e656160bdd2d2a512055
GeopJr <evan@geopjr.dev>
2022-05-13 16:04:36 +0300

    chore: get rid of prerequisites.cr

--- src/collision.cr
+++ src/collision.cr
@@ -3,6 +3,28 @@ require "compiled_license"
 require "gettext"
 require "log"
 
+module Collision
+  extend self
+
+  LOGGER = Log.for("Collision", ARGV[0]? == "--debug" ? Log::Severity::Debug : Log::Severity::Warn)
+
+  begin
+    Gettext.setlocale(Gettext::LC::ALL, "")
+    Gettext.bindtextdomain("dev.geopjr.Collision", {{env("COLLISION_LOCALE_LOCATION").nil? ? "/usr/share/locale" : env("COLLISION_LOCALE_LOCATION")}})
+    Gettext.textdomain("dev.geopjr.Collision")
+  rescue ex
+    LOGGER.debug { ex }
+  end
+
+  HASH_FUNCTIONS = ["MD5", "SHA1", "SHA256", "SHA512"]
+  VERSION        = {{read_file("./shard.yml").split("version: ")[1].split("\n")[0]}} # Shards binary might not be in PATH, reading yml is safer
+
+  ARTICLE = Gettext.gettext("https://en.wikipedia.org/wiki/Comparison_of_cryptographic_hash_functions")
+
+  RESOURCE = Gio::Resource.new_from_data(GLib::Bytes.new_take({{read_file("./data/dev.geopjr.Collision.gresource")}}.bytes))
+  RESOURCE._register
+end
+
 require "./collision/functions/*"
 require "./collision/views/*"
 require "./collision/views/tools/*"
