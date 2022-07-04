9a1e558f716d92e8b23586698ca5ff2b3e82dcf4
Evangelos Paterakis <evan@geopjr.dev>
2022-07-04 13:58:24 +0300

    chore(about_dialog): bump copyright date

--- src/collision/functions/about_action.cr
+++ src/collision/functions/about_action.cr
@@ -6,17 +6,19 @@ module Collision
     app.add_action(action)
 
     action.activate_signal.connect do
-      Gtk.show_about_dialog(APP.active_window, name: "About Collision",
+      Gtk.show_about_dialog(
+        APP.active_window, name: "About Collision",
         application: APP,
         program_name: Gettext.gettext("Collision"),
         version: VERSION,
         logo_icon_name: "dev.geopjr.Collision",
-        copyright: "© 2021 Evangelos Paterakis",
+        copyright: "© 2021-2022 Evangelos Paterakis",
         website: "https://github.com/GeopJr/Collision",
         authors: ["Evangelos \"GeopJr\" Paterakis"],
         artists: ["Tobias Bernard", "Evangelos \"GeopJr\" Paterakis"],
         translator_credits: Gettext.gettext("translator-credits"),
-        license_type: Gtk::License::Bsd)
+        license_type: Gtk::License::Bsd
+      )
     end
   end
 end
