190b6026538795c25fc9eaa01867ca4a80965b5a
Evangelos Paterakis <evan@geopjr.dev>
2022-09-20 05:04:19 +0300

    feat: add DND controller to the compare button

--- src/collision/views/tools/compare.cr
+++ src/collision/views/tools/compare.cr
@@ -3,34 +3,45 @@ module Collision
     extend self
 
     def init
+      TOOL_COMPARE_BUTTON.add_controller(Collision::DragNDrop.new(Collision::Compare).controller)
+
       TOOL_COMPARE_BUTTON.clicked_signal.connect do
         TOOL_COMPARE_FILE_CHOOSER_NATIVE.show
       end
 
       TOOL_COMPARE_FILE_CHOOSER_NATIVE.response_signal.connect do |response|
         next unless response == -3
-        LOGGER.debug { "Begin comparing tool" }
 
-        TOOL_COMPARE_BUTTON_SPINNER.visible = true
-        TOOL_COMPARE_BUTTON_IMAGE.visible = false
-        TOOL_COMPARE_BUTTON.remove_css_class("success")
-        TOOL_COMPARE_BUTTON.remove_css_class("error")
+        Collision::Compare.file = TOOL_COMPARE_FILE_CHOOSER_NATIVE.file.not_nil!
+      rescue ex
+        LOGGER.debug { ex }
+      end
+    end
 
-        TOOL_COMPARE_BUTTON_LABEL.label = TOOL_COMPARE_FILE_CHOOSER_NATIVE.file.not_nil!.path.not_nil!.basename.to_s
-        Collision.handle_spawning do
-          compareFileSHA256 = Collision.calculate_hash("sha256", TOOL_COMPARE_FILE_CHOOSER_NATIVE.file.not_nil!.path.to_s)
-          result = CLIPBOARD_HASH["SHA256"] == compareFileSHA256
-          classes = Collision.class(result)
+    def file=(file : Gio::File)
+      LOGGER.debug { "Begin comparing tool" }
 
-          sleep 500.milliseconds
-          TOOL_COMPARE_BUTTON_SPINNER.visible = false
-          TOOL_COMPARE_BUTTON_IMAGE.visible = true
-          TOOL_COMPARE_BUTTON_IMAGE.icon_name = Collision.icon(result)
-          TOOL_COMPARE_BUTTON.add_css_class(classes[:add])
-          TOOL_COMPARE_BUTTON.remove_css_class(classes[:remove])
+      Collision.file?(file.path.not_nil!)
 
-          LOGGER.debug { "Finished comparing tool" }
-        end
+      TOOL_COMPARE_BUTTON_SPINNER.visible = true
+      TOOL_COMPARE_BUTTON_IMAGE.visible = false
+      TOOL_COMPARE_BUTTON.remove_css_class("success")
+      TOOL_COMPARE_BUTTON.remove_css_class("error")
+
+      TOOL_COMPARE_BUTTON_LABEL.label = file.path.not_nil!.basename.to_s
+      Collision.handle_spawning do
+        compareFileSHA256 = Collision.calculate_hash("sha256", file.path.to_s)
+        result = CLIPBOARD_HASH["SHA256"] == compareFileSHA256
+        classes = Collision.class(result)
+
+        sleep 500.milliseconds
+        TOOL_COMPARE_BUTTON_SPINNER.visible = false
+        TOOL_COMPARE_BUTTON_IMAGE.visible = true
+        TOOL_COMPARE_BUTTON_IMAGE.icon_name = Collision.icon(result)
+        TOOL_COMPARE_BUTTON.add_css_class(classes[:add])
+        TOOL_COMPARE_BUTTON.remove_css_class(classes[:remove])
+
+        LOGGER.debug { "Finished comparing tool" }
       end
     end
   end
