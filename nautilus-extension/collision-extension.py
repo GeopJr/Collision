f8bd404951f3ac4a15d7df7efafb5f5c2b813442
DodoLeDev <dorianblanchet1@gmail.com>
2022-11-22 00:09:23 +0100

    Added support for the background rick-click event  - Introduces a unclickable entry (because no file is selected)  - Fixes the 'object not initialized' bug

--- nautilus-extension/collision-extension.py
+++ nautilus-extension/collision-extension.py
@@ -21,13 +21,21 @@ class NautilusCollision(Nautilus.MenuProvider, GObject.GObject):
     def openWithCollision(self, menu, files):  # Executed method when the right-click entry is clicked
         file_path = unquote(urlparse(files[0].get_uri()).path)
         Popen(["collision", file_path])  # Collision need to be in your PATH
+    
+    def get_background_items(self, files):  # Add a unclickable entry if the background is right-clicked (fixes the 'object not initialized' bug)
+        menu_item = Nautilus.MenuItem(
+                        name="NautilusCollision::NoFileSelected",
+                        label="Hash not available",
+                        sensitive=False)
+
+        return menu_item,
 
     def get_file_items(self, files):
         if len(files) > 1:  # The option doesn't appear when there is more than 1 file selected (because Collision doesn't handle it yet)
             return ()
 
         menu_item = Nautilus.MenuItem(
-                        name="collision",
+                        name="NautilusCollision::CheckHashes",
                         label="Check Hashes")  # TODO : Add the ability to make this label translated with the .po files
 
         menu_item.connect('activate', self.openWithCollision, files)
