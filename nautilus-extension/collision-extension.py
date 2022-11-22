f46730ead6260747de6aa32eb8d673b0d7c9708b
Evangelos Paterakis <evan@geopjr.dev>
2022-11-22 17:13:18 +0200

    fix: use repr to wrap file path

--- nautilus-extension/collision-extension.py
+++ nautilus-extension/collision-extension.py
@@ -2,8 +2,8 @@
 Add a Collision shortcut button to the right-click menu (Nautilus GTK4)
 """
 
-from subprocess import Popen, check_call  # In order to open Collision on a new process. If not, Nautilus will freeze while the program is open
-from urllib.parse import urlparse, unquote  # Necessary to parse file URI
+from subprocess import Popen, check_call
+from urllib.parse import urlparse, unquote
 from shutil import which
 from gi import require_version
 from gettext import textdomain, gettext
@@ -35,7 +35,7 @@ class NautilusCollision(Nautilus.MenuProvider, GObject.GObject):
     
     # Executed method when the right-click entry is clicked
     def openWithCollision(self, menu, files):
-        file_path = "\"" + unquote(urlparse(files[0].get_uri()).path) + "\""
+        file_path = repr(unquote(urlparse(files[0].get_uri()).path))
         if self.collision != "collision":
             file_path = "@@ " + file_path + " @@"
         Popen(self.collision + " " + file_path, shell=True)  # Collision need to be in your PATH
@@ -44,7 +44,7 @@ class NautilusCollision(Nautilus.MenuProvider, GObject.GObject):
         return
 
     def get_file_items(self, files):
-        # The option doesn't appear when there is more than 1 file selected (because Collision doesn't handle it yet) or when a folder is selected
+        # The option doesn't appear when there is more than 1 file selected or when a folder is selected
         if len(files) > 1 or (len(files) == 1 and files[0].is_directory()) or self.collision == False: 
             return ()
 
