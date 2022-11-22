8b4e244de9a915aab1e1eccaef756faef255c703
Evangelos Paterakis <evan@geopjr.dev>
2022-11-22 03:02:44 +0200

    feat: support flatpak
    fix: check if a collision is at all available

--- nautilus-extension/collision-extension.py
+++ nautilus-extension/collision-extension.py
@@ -1,10 +1,10 @@
 """
 Add a Collision shortcut button to the right-click menu (Nautilus GTK4)
-
 """
 
-from subprocess import Popen  # In order to open Collision on a new process. If not, Nautilus will freeze while the program is open
+from subprocess import Popen, check_call  # In order to open Collision on a new process. If not, Nautilus will freeze while the program is open
 from urllib.parse import urlparse, unquote  # Necessary to parse file URI
+from shutil import which
 from gi import require_version
 
 require_version('Gtk', '4.0')
@@ -12,26 +12,34 @@ require_version('Nautilus', '4.0')
 
 from gi.repository import Nautilus, GObject, Gtk, Gdk
 
-class NautilusCollision(Nautilus.MenuProvider, GObject.GObject):
+def get_collision():
+    try:
+        check_call("flatpak list --columns=application | grep \"dev.geopjr.Collision\" &> /dev/null", shell=True)
+        return "flatpak run --file-forwarding dev.geopjr.Collision"
+    except subprocess.CalledProcessError:
+        if which("collision") is not None:
+            return "collision"
+        else:
+            return False
 
-    def __init__(self):  # The constructor seems to be recquired, but I don't have anything to set up xD
+class NautilusCollision(Nautilus.MenuProvider, GObject.GObject):
+    collision = get_collision()
+
+    def __init__(self):
         self.window = None
         return
     
-    def openWithCollision(self, menu, files):  # Executed method when the right-click entry is clicked
-        file_path = unquote(urlparse(files[0].get_uri()).path)
-        Popen(["collision", file_path])  # Collision need to be in your PATH
+    # Executed method when the right-click entry is clicked
+    def openWithCollision(self, menu, files):
+        file_path = "\"" + unquote(urlparse(files[0].get_uri()).path) + "\""
+        if self.collision != "collision":
+            file_path = "@@ " + file_path + " @@"
+        Popen(self.collision + " " + file_path, shell=True)  # Collision need to be in your PATH
     
-    def get_background_items(self, files):  # Add a unclickable entry if the background is right-clicked (fixes the 'object not initialized' bug)
-        menu_item = Nautilus.MenuItem(
-                        name="NautilusCollision::NoFileSelected",
-                        label="Hash not available",
-                        sensitive=False)
-
-        return menu_item,
 
     def get_file_items(self, files):
-        if len(files) > 1 or (len(files) == 1 and files[0].is_directory()):  # The option doesn't appear when there is more than 1 file selected (because Collision doesn't handle it yet) or when a folder is selected
+        # The option doesn't appear when there is more than 1 file selected (because Collision doesn't handle it yet) or when a folder is selected
+        if len(files) > 1 or (len(files) == 1 and files[0].is_directory()) or self.collision == False: 
             return ()
 
         menu_item = Nautilus.MenuItem(
